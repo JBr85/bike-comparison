612536b7748e84039ba740e2ae0f24aaa7869517
JBr85 <james.brightwell@gmail.com>
2025-04-10 20:03:28 +0100

    full committ

---
+++ frontend/build/precache-manifest.91236aa98722c49d2fc999016d34f37a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c695fed47e4a992ff5a297c9b4d8afe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aba2c8be13568a33c9a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "035c83450a4e354ad863",
+    "url": "/static/js/2.16d0efc1.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "9aba2c8be13568a33c9a",
+    "url": "/static/js/main.3d5fb87c.chunk.js"
+  },
+  {
+    "revision": "a50475a1f486eef27c5a",
+    "url": "/static/js/runtime~main.4414e1f1.js"
+  }
+]);
