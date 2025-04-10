ce5b1f268bc0a40cf5bbc9d09373bfd975cec7fe
JBr85 <james.brightwell@gmail.com>
2025-04-10 23:02:12 +0100

    updated node.js

---
+++ frontend/build/precache-manifest.87509e73cd4a24480801b6050c780e6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c9844c523ef87d9f17891335e540f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b82b996449c62d9e462",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "035c83450a4e354ad863",
+    "url": "/static/js/2.16d0efc1.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "9b82b996449c62d9e462",
+    "url": "/static/js/main.d4312bff.chunk.js"
+  },
+  {
+    "revision": "a50475a1f486eef27c5a",
+    "url": "/static/js/runtime~main.4414e1f1.js"
+  }
+]);
