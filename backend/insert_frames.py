d32f6945f85c2fbe52e2fb803a7f7596444ceba2
JBr85 <james.brightwell@gmail.com>
2025-05-20 10:44:23 +0100

    Updated database to include Stromm frames

--- backend/insert_frames.py
+++ backend/insert_frames.py
@@ -5,12 +5,10 @@ db = SessionLocal()
 
 # List of new frames to insert
 frames = [
-    {"make": "Dolan DF5", "size": "51.5", "reach": 387.6, "stack": 475.3, "top_tube": 515, "head_angle": 72, "seat_angle": 75, "head_tube": 90, "chainstay": None, "wheelbase": None, "front_centre": None, "bb_drop": None, "fork_rake": None},
-    {"make": "Dolan DF5", "size": "53", "reach": 400.1, "stack": 484.9, "top_tube": 530, "head_angle": 72.5, "seat_angle": 75, "head_tube": 100, "chainstay": None, "wheelbase": None, "front_centre": None, "bb_drop": None, "fork_rake": None},
-    {"make": "Dolan DF5", "size": "55", "reach": 417.5, "stack": 494.5, "top_tube": 550, "head_angle": 73.5, "seat_angle": 75, "head_tube": 110, "chainstay": None, "wheelbase": None, "front_centre": None, "bb_drop": None, "fork_rake": None},
-    {"make": "Dolan DF5", "size": "57", "reach": 435, "stack": 504.1, "top_tube": 570, "head_angle": 74, "seat_angle": 75, "head_tube": 120, "chainstay": None, "wheelbase": None, "front_centre": None, "bb_drop": None, "fork_rake": None},
-    {"make": "Dolan DF5", "size": "59", "reach": 449.4, "stack": 524.7, "top_tube": 590, "head_angle": 74.5, "seat_angle": 75, "head_tube": 140, "chainstay": None, "wheelbase": None, "front_centre": None, "bb_drop": None, "fork_rake": None},
-    {"make": "Dolan DF5", "size": "61", "reach": 453.6, "stack": 545.5, "top_tube": 610, "head_angle": 75, "seat_angle": 74, "head_tube": 160, "chainstay": None, "wheelbase": None, "front_centre": None, "bb_drop": None, "fork_rake": None}
+    {"make": "Stromm Track", "size": "Small", "reach": 400, "stack": 480, "top_tube": None, "head_angle": 73.5, "seat_angle": 74, "head_tube": None, "chainstay": None, "wheelbase": 940.7, "front_centre": 565.7, "bb_drop": None, "fork_rake": 40},
+    {"make": "Stromm Track", "size": "Medium", "reach": 425, "stack": 500, "top_tube": None, "head_angle": 73.5, "seat_angle": 74, "head_tube": None, "chainstay": None, "wheelbase": 971.6, "front_centre": 596.6, "bb_drop": None, "fork_rake": 40},
+    {"make": "Stromm Track", "size": "Large", "reach": 450, "stack": 520, "top_tube": None, "head_angle": 73.5, "seat_angle": 74, "head_tube": None, "chainstay": None, "wheelbase": 1002.6, "front_centre": 627.6, "bb_drop": None, "fork_rake": 40},
+    {"make": "Stromm Track", "size": "Extra Large", "reach": 466, "stack": 540, "top_tube": None, "head_angle": 73.5, "seat_angle": 74, "head_tube": None, "chainstay": None, "wheelbase": 1024.5, "front_centre": 649.5, "bb_drop": None, "fork_rake": 40}
 ]
 
 # Insert frames into the database
@@ -22,4 +20,4 @@ for frame_data in frames:
 db.commit()
 db.close()
 
-print("✅ Dolan DF5 frames inserted successfully!")
+print("✅ Stromm frames inserted successfully!")
